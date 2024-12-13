28710b581f8386eb2dc752717ac1a748f74f5a4e
Shreyansh Priyadarshi <shreyansh12361@gmail.com>
2024-12-14 00:58:04 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
+import Login from './pages/login'
 import Register from './pages/Register'
 import PrivateRoute from './components/PrivateRoute'
 import Home from './pages/Home'
